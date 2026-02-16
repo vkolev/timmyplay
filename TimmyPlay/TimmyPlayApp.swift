2094ed0dc52eddae2939fa3ac652772ec70490e1
Vladimir Kolev <vladimir.r.kolev@gmail.com>
2026-02-16 10:04:38 +0100

    Initial Commit

---
+++ TimmyPlay/TimmyPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimmyPlayApp.swift
+//  TimmyPlay
+//
+//  Created by Vladimir Kolev on 16.02.26.
+//
+
+import SwiftUI
+
+@main
+struct TimmyPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
