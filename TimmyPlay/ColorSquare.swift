a5818d2c7b6ec6b3751abf79d1473fc1dc0335ce
Vladimir Kolev <vladimir.r.kolev@gmail.com>
2026-02-17 07:13:45 +0100

    Fix: Animation and elevation of buttons

--- TimmyPlay/ColorSquare.swift
+++ TimmyPlay/ColorSquare.swift
@@ -14,7 +14,7 @@ struct ColorSquare: View {
     let frameSize: CGFloat
     let position: GridPosition
     
-    @State private var isPressed = false
+    @GestureState private var isPressed = false
     
     var body: some View {
         RoundedRectangle(cornerRadius: 20)
@@ -29,24 +29,30 @@ struct ColorSquare: View {
             .frame(width: frameSize, height: frameSize)
             .overlay(
                 RoundedRectangle(cornerRadius: 20)
-                    .stroke(Color.white.opacity(0.4), lineWidth: 1)
+                    .stroke(Color.white.opacity(0.7), lineWidth: 4)
                     .blendMode(.overlay)
             )
-            .shadow(color: .black.opacity(0.25), radius: isPressed ? 2 : 6, x: 0, y: isPressed ? 1 : 4)
+            .shadow(color: .black.opacity(0.30), radius: isPressed ? 4 : 16, x: 0, y: isPressed ? 2 : 8)
             .scaleEffect(isPressed ? 0.95 : 1)
             .animation(.spring(response: 0.25, dampingFraction: 0.6), value: isPressed)
-            .onTapGesture {pGesture in
-                if (color == .gray) {
-                    // play sound oh-oh
-                    playSound(named: "icq-sound.mp3")
-                } else {
-                    // play sound yeah
-                    playSound(named: "pop-sound.mp3")
-                    gamePoints
-                        .removeAll { $0 == position}
-                    color = .gray
-                }
-            }
+            .gesture(
+                 LongPressGesture(minimumDuration: 0.2)
+                    .updating($isPressed) { currentState, gestureState, transaction in
+                        gestureState = currentState
+                    }
+                    .onEnded { _ in
+                        if (color == .gray) {
+                            // play sound oh-oh
+                            playSound(named: "icq-sound.mp3")
+                        } else {
+                            // play sound yeah
+                            playSound(named: "pop-sound.mp3")
+                            gamePoints
+                                .removeAll { $0 == position}
+                            color = .gray
+                        }
+                    }
+            )
         
     }
     
@@ -66,4 +72,23 @@ struct ColorSquare: View {
             print("Error playing sound: \(error.localizedDescription)")
         }
     }
+}
+
+#Preview {
+    struct Preview: View {
+        @State var gamePoints = [GridPosition(row: 0, col: 1), GridPosition(row: 2, col: 3)]
+        @State var gridSize = 4
+        @State var gameState = GameState.playing
+        var body: some View {
+            
+            ColorSquare(
+                color: .pink,
+                gamePoints: $gamePoints,
+                frameSize: 300,
+                position: GridPosition(row: 0, col: 0)
+            )
+        }
+    }
+    
+    return Preview()
 }
